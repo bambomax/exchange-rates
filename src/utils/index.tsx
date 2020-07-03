faff3d565d7ec60cc441ad14e92b554a5a86717f
Max_Onyshchenko <maxonyschenko94@gmail.com>
2020-07-03 23:10:57 +0300

    sorted data

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -3,8 +3,17 @@ import { colors } from '../constants/colors'
 // types
 import { IData, IChartData, IChartDataItem } from '../App'
 
+import moment from 'moment'
+
 export const getRandomColor = () => colors[Math.floor(Math.random() * 10)]
 
-export const transformFetchedData = ({ rates }: IData): IChartData =>
-  Object.entries(rates).reduce((acc, [date, currencyRates]) =>
+export const transformFetchedData = ({ rates }: IData): IChartData => {
+  const entries = Object.entries(rates)
+
+  const result = entries.reduce((acc, [date, currencyRates]) =>
     [...acc, { date, ...currencyRates } as IChartDataItem], [] as IChartData)
+
+  result.sort((a, b) => moment(a.date).isAfter(moment(b.date)) ? 1 : -1)
+
+  return result
+}
